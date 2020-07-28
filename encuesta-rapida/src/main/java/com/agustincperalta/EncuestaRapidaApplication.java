079dd4829edffed95b2f27f487cc3d4f2ada9232
Agustin Peralta <jnrp@gft.com>
2020-07-28 12:09:18 -0500

    Estructura inicial

---
+++ encuesta-rapida/src/main/java/com/agustincperalta/EncuestaRapidaApplication.java
@@ -0,0 +1,13 @@
+package com.agustincperalta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestaRapidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestaRapidaApplication.class, args);
+	}
+
+}
---
+++ encuesta-rapida/src/test/java/com/agustincperalta/EncuestaRapidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustincperalta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestaRapidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
